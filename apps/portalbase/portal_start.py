b3ab384c12944f9ff51b87f69f2560d51c6dd272
Reem Khamis <khamisr@codescalers.com>
2017-05-09 17:14:42 +0200

    Make sure to patch subprocess when using gevent

--- apps/portalbase/portal_start.py
+++ apps/portalbase/portal_start.py
@@ -1,12 +1,13 @@
 # this must be in the beginning so things are patched before ever imported by other libraries
 from gevent import monkey
+monkey.patch_subprocess()
 monkey.patch_socket()
 monkey.patch_ssl()
 monkey.patch_thread()
 monkey.patch_time()
 
 from js9 import j
-import JumpScale9Portal.portal
+import JumpScale.portal
 import click
 
 @click.group(invoke_without_command=True)
@@ -22,8 +23,8 @@ def cli(ctx, instance):
 @click.option('--instance', default='main', help='instance of portal')
 def start(ctx, instance):
     instance = instance or ctx.obj.get('INSTANCE')
-    cfg = j.data.serializer.yaml.load('%s/portals/%s/config.yaml' % (j.dirs.CFGDIR, instance))
-    j.application.instanceconfig = cfg
+    hrd = j.data.hrd.get('%s/portals/%s/config.hrd' % (j.dirs.cfgDir, instance))
+    j.application.instanceconfig = hrd
 
     j.application.start("portal")
 
