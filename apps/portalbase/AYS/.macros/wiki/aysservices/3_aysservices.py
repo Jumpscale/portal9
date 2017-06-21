117769207523bb2ac678ae95b78e460e80f0590e
Ahmed T. Youssef <xmonader@gmail.com>
2017-06-21 14:21:51 +0200

    support treeview for services

--- apps/portalbase/AYS/.macros/wiki/aysservices/3_aysservices.py
+++ apps/portalbase/AYS/.macros/wiki/aysservices/3_aysservices.py
@@ -1,4 +1,42 @@
 
+def svkey(sv):
+    return sv['role'] + "!" + sv['name']
+
+
+def svdetailslink(sv):
+    return "Service?aysrole={}&aysname={}&reponame={}".format(sv['role'], sv['name'], sv['repository'])
+
+
+def flatToTree(servicesdict):
+    """
+    Takes in a flat services dict and outputs a structured tree to be rendered by bootstrap-treeview.
+
+    @param servicesdict dict: servicekey mapped to servicename
+
+    @returns tree services.
+
+    """
+
+    processed = set()
+
+    def processService(key):
+        service = servicesdict[key]
+        processed.add(key)
+        children = []
+        for c in service.get('children', []):
+            children.append(servicesdict[svkey(c)])
+
+        data = {'text': key, 'href': svdetailslink(service), 'nodes': [processService(svkey(c)) for c in children]}
+
+        return data
+
+    tree = []
+
+    for key, service in servicesdict.items():
+        if key not in processed:
+            tree.append(processService(key))
+    return tree
+
 
 def main(j, args, params, tags, tasklet):
     try:
@@ -8,7 +46,13 @@ def main(j, args, params, tags, tasklet):
         client = aysactor.get_client(ctx=ctx)
 
         services = client.listServices(reponame).json()
-        args.doc.applyTemplate({'services': services, 'reponame': reponame})
+        fullservicesinfo = {}
+        for s in services:
+            name, role = s['name'], s['role']
+            fullservicesinfo[svkey(s)] = (client.getServiceByName(name, role, reponame).json())
+        servicestree = flatToTree(fullservicesinfo)
+        args.doc.applyTemplate({'services': services, 'reponame': reponame, 'servicestree': servicestree})
+
     except Exception as e:
         args.doc.applyTemplate({'error': str(e)})
 
