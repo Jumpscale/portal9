f1d65715cf617994da37ae92ed01ae081c53e9bc
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-21 09:52:11 +0200

    revert un-need change to portal install

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #for development mode
 apt-get install -y  libsnappy-dev
 pip3 install -e .
+
+# install portal
+js9 'j.tools.prefab.get().apps.portal.install()'
--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# install portal
-js9 'j.tools.prefab.get().apps.portal.install()'
-
 # start portal
 js9 'j.tools.prefab.get().apps.portal.start()'
