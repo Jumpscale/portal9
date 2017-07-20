e2010129f1abe11150f08bfc600e7aad4ec06a00
Ahmed T. Youssef <xmonader@gmail.com>
2017-07-20 09:48:21 +0300

    Issuemanagerfixes (#46)
    * port addAccordion to portal9
    
    * add jqwidgets depdencey
    
    * sort the panels by title for issue manager

--- JumpScale9Portal/portal/docgenerator/PageHTML.py
+++ JumpScale9Portal/portal/docgenerator/PageHTML.py
@@ -1044,6 +1044,53 @@ function copyText$id() {
     def addHTMLBody(self, body):
         self.body += body
 
+    def addAccordion(self, panels):
+        self.addJS('/jslib/codemirror/autorefresh.js', header=False)
+        self.addMessage('<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">')
+
+        panels.sort(key=lambda x: x['title'])
+
+        for panel_data in panels:
+            if panel_data is None:
+                continue
+
+            content = panel_data['content']
+
+            for item in ['header_id', 'section_id', 'label_id', 'label_icon', 'label_color']:
+                if item not in panel_data:
+                    panel_data[item] = j.data.idgenerator.generateXCharID(10)
+
+            self.addMessage("""
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="%(header_id)s">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#%(section_id)s" aria-expanded="true" aria-controls="%(section_id)s">%(title)s</a>
+            """ % panel_data)
+
+            if 'label_content' in panel_data:
+                self.addMessage("""
+                <a id=%(label_id)s class="label-archive label label-%(label_color)s glyphicon glyphicon glyphicon-%(label_icon)s pull-right">%(label_content)s</a>
+                """ % panel_data)
+
+            self.addMessage("""
+                </h4>
+              </div>
+              <div id="%(section_id)s" class="panel-collapse collapse" role="tabpanel" aria-labelledby="%(header_id)s">
+                <div class="panel-body">
+                """ % panel_data)
+
+            if panel_data.get('code', False):
+                self.addCodeBlock(content, edit=False, exitpage=True, spacename='', pagename='', linenr=True, autorefresh=True)
+            else:
+                self.addMessage(content)
+
+            self.addMessage("""
+                </div> <!-- panel body-->
+              </div> <!-- panel collapse-->
+            </div> <!-- panel default-->""")
+
+        self.addMessage('</div>')  # close panel-group
+
     def getContent(self):
         return str(self)
 
