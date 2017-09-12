95b3b7d52fbadb34567619b7b193e771039825dc
Abdelrahman Hussein <husseina@greenitglobe.com>
2017-09-12 11:33:23 +0200

    update test/build configs

--- upload_image.sh
+++ upload_image.sh
@@ -4,3 +4,16 @@
 
 sudo docker login -u ${DOCKERHUB_LOGIN} -p ${DOCKERHUB_PASS}
 sudo docker push jumpscale/portal9
+
+#!/bin/bash
+
+# uploads the successfully built portal9 image to dockr hub for development re-use
+
+if [ -n $TRAVIS_EVENT_TYPE ] && [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+    image_id=$(sudo docker images -q jumpscale/portal9)
+    sudo docker tag $image_id jumpscale/portal9nightly:latest
+fi
+
+sudo docker login -u ${DOCKERHUB_LOGIN} -p ${DOCKERHUB_PASS}
+sudo docker push jumpscale/portal9nightly
+
