72f15964a36e8b5f3c8eacbe86481d9df820e1f0
Reem Khamis <khamisr@codescalers.com>
2018-06-24 16:29:06 +0200

    Make the pie charts a little nicer

--- JumpScale9Portal/portal/docgenerator/templates/pie.js
+++ JumpScale9Portal/portal/docgenerator/templates/pie.js
@@ -7,7 +7,10 @@ pie.Set("chart.linewidth", 1);
 pie.Set("chart.exploded", 3);
 pie.Set("chart.variant", "{donut}");
 pie.Set("chart.key", {pieLegend});
-pie.Set("chart.key.position", "graph");
+pie.Set("chart.key.position", "gutter");
+pie.Set("chart.radius", {radius})
+pie.Set("chart.key.position.y", {gutterPosition})
+
 
 pie.Set("chart.colors", ['#BCD3EB', '#1B3B44', '#536270', '#6B93AB', '#F2F2F8', '#568E8B', '#E4DA9C', '#CE899E', '#265356', '#723642', '#B0B0AE', '#4F3244', '#8892B6'])
 pie.Set('chart.tooltips.effect', 'fade');
